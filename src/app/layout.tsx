bd45b4404f3f7c053d5f73a9a0e52e457239129b
Kazi Mustakim <kazimustakim39@gmail.com>
2025-08-02 14:28:06 +0000

    ok let add smooth scrolling  animation for whole project ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from '@/components/layout/Navbar';
 import { Toaster } from "@/components/ui/toaster";
 import { Footer } from '@/components/layout/Footer';
 import { CustomCursor } from '@/components/layout/CustomCursor';
+import { SmoothScroll } from '@/components/layout/SmoothScroll';
 
 export const metadata: Metadata = {
   title: 'Mustakim Kazi - Creative Developer',
@@ -24,13 +25,15 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={`font-sans antialiased grainy-texture`}>
-        <CustomCursor />
-        <Navbar />
-        <main className="flex-1 flex flex-col">
-          {children}
-        </main>
-        <Footer />
-        <Toaster />
+        <SmoothScroll>
+          <CustomCursor />
+          <Navbar />
+          <main className="flex-1 flex flex-col">
+            {children}
+          </main>
+          <Footer />
+          <Toaster />
+        </SmoothScroll>
       </body>
     </html>
   );
